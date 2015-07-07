cb1dbf7b2c8d7f94116fc9e7963131dc502df98e
Pascal Berger <pascal.berger@ioz.ch>
2015-07-07 11:55:55 +0200

    Bump version number to 0.1.2

--- docroot/breeze-kendo.js
+++ docroot/breeze-kendo.js
@@ -4,7 +4,7 @@
  * This is a forked version of the breeze-kendo bridge (https://github.com/kendo-labs/breeze-kendo).
  * You can get the latest version at https://github.com/iozag/breeze-kendo.
  *
- * Version 0.1.1
+ * Version 0.1.2
  *
  * Released under Apache 2.0 licence (http://kendoui-labs.telerik.com/#license)
  *
